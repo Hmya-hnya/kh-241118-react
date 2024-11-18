b86a159a53a50c6b00a5ff7912318775bb17bea3
hmya_hnya <kh120205@gmail.com>
2024-11-18 11:22:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// [JSX 문법 이해하기]
+// JSX에 표현식 포함하기: {변수나 간단한 연산식}
+// 감싸인 요소로 표현하기: 컴포넌트를 반환 시 하나의 태그이어야 함.
 
 function App() {
+  const name = "곰돌이사육사";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name}의 리액트 공부를 시작 하겠습니다.</h1>
+      <p>리액트는 JSX문법을 사용하고 리턴문 내에서 작성 합니다.</p>
+    </>
   );
 }
 
