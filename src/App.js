1d58935f222e9873c7e971aa16dd2a925ab3b55b
hmya_hnya <kh120205@gmail.com>
2024-11-18 12:40:13 +0900

    241118am

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-// [JSX 문법 이해하기]
-// JSX에 표현식 포함하기: {변수나 간단한 연산식}
-// 감싸인 요소로 표현하기: 컴포넌트를 반환 시 하나의 태그이어야 함.
+import JsxSyntax from "./jsx문법";
+import GreetingComponent from "./GreetingComponent";
 
 function App() {
-  const name = "곰돌이사육사";
   return (
     <>
-      <h1>{name}의 리액트 공부를 시작 하겠습니다.</h1>
-      <p>리액트는 JSX문법을 사용하고 리턴문 내에서 작성 합니다.</p>
+      <h1>여기는 App.js입니다.</h1>
+      <JsxSyntax />
+      <GreetingComponent />
     </>
   );
 }
